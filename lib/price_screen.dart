4ad4ecc03005ce541db19b1204a26962c38ac6ea
giusoliva <64439473+giusoliva@users.noreply.github.com>
2020-05-15 16:04:59 +0200

    added DropDownMenu

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PriceScreen extends StatefulWidget {
@@ -6,6 +7,8 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'a';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +45,23 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+                value: selectedCurrency,
+                items: [
+                  DropdownMenuItem<String>(
+                    value: 'a',
+                    child: Text('a'),
+                  ),
+                  DropdownMenuItem<String>(
+                    value: 'b',
+                    child: Text('b'),
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    selectedCurrency = value;
+                  });
+                }),
           ),
         ],
       ),
